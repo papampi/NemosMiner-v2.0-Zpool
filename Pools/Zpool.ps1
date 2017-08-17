721ca26b910fb12155ca4a2410e553c1cb51a253
Payam Nab <papampi@users.noreply.github.com>
2017-08-17 23:08:36 +0430

    Fix new x11, qubit and quark
    NiceHash changed so did Zpool!!!

--- Pools/Zpool.ps1
+++ Pools/Zpool.ps1
@@ -25,10 +25,13 @@ $Zpool_Request | Get-Member -MemberType NoteProperty | Select -ExpandProperty Na
 	
     switch($Zpool_Algorithm)
     {
-        "equihash"{$Divisor /= 1000}
-        "blake2s"{$Divisor *= 1000}
-		"blakecoin"{$Divisor *= 1000}
-        "decred"{$Divisor *= 1000}
+        "equihash"	{$Divisor /= 1000}
+        "blake2s"	{$Divisor *= 1000}
+	"blakecoin"	{$Divisor *= 1000}
+        "decred"	{$Divisor *= 1000}
+	"X11"		{$Divisor *= 1000}
+        "qubit"		{$Divisor *= 1000}
+        "quark"		{$Divisor *= 1000}
     }
 
     if((Get-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit" -Value ([Double]$Zpool_Request.$_.estimate_last24h/$Divisor)}
